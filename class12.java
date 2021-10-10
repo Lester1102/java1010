13a31695f1d37f974d434ebee17a08463c78d6d5
Lester1102 <91860948+Lester1102@users.noreply.github.com>
2021-10-10 21:34:54 +0800

    Add files via upload

---
+++ class12.java
@@ -0,0 +1,24 @@
+class CWin 
+{  int width; int height; 
+ String name; 
+ void setW(int w) {}  
+ void setH(int h) {} 
+ void setName(String s) 
+ {name=s;} 
+ public void show() 
+ { 
+ System.out.println("Name="+name); 
+ System.out.println("W="+width+", H="+height); 
+ } 
+ } 
+ public class class12
+ { 
+ public static void main(String args[]) 
+ { 
+ CWin cw=new CWin(); 
+ cw.setName("My Windows"); 
+ cw.setW(5); 
+ cw.setH(3); 
+ cw.show(); 
+ } 
+ }
